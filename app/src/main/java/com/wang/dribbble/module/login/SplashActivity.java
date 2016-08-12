c9222193b415104dbc785a675bd6d4e06f83de09
whlong <81813780@qq.com>
2016-08-12 11:28:35 +0800

    Add splash anim with logo .

--- app/src/main/java/com/wang/dribbble/module/login/SplashActivity.java
+++ app/src/main/java/com/wang/dribbble/module/login/SplashActivity.java
@@ -1,5 +1,8 @@
 package com.wang.dribbble.module.login;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,26 +11,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.tumblr.remember.Remember;
 import com.wang.dribbble.R;
 import com.wang.dribbble.module.base.BaseActivity;
 import com.wang.dribbble.module.main.MainActivity;
-import com.wang.dribbble.utils.ImageSize;
-
-import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
-import rx.Observable;
-import rx.functions.Action1;
 
 /**
  * Created by Jack Wang on 2016/6/2.
  */
 public class SplashActivity extends BaseActivity {
-
-    public static final String SPLASH_PHOTO="https://unsplash.it/480/800/?random";
 
     @BindView(R.id.mSplashPhoto)
     ImageView mSplashPhoto;
@@ -49,24 +43,19 @@ public class SplashActivity extends BaseActivity {
             mLoginBut.setVisibility(View.VISIBLE);
             return;
         }
-        Observable.timer(3, TimeUnit.SECONDS)
-                .subscribe(new Action1<Long>() {
-                    @Override
-                    public void call(Long aLong) {
-                        startActivity(new Intent(getActivity(),MainActivity.class));
-                        finish();
-                    }
-                });
     }
 
     private void showSplashPhoto() {
-        Glide.with(getActivity())
-                .load(SPLASH_PHOTO)
-                .skipMemoryCache(true)
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .placeholder(R.color.placeholder)
-                .override(ImageSize.FULL[0], ImageSize.FULL[1])
-                .into(mSplashPhoto);
+        ObjectAnimator alphaAnim=ObjectAnimator.ofFloat(mSplashPhoto,"alpha",0,1);
+        alphaAnim.setDuration(3000);
+        alphaAnim.start();
+        alphaAnim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                startActivity(new Intent(getActivity(),MainActivity.class));
+                finish();
+            }
+        });
     }
 
     public void showLoginUi(View view) {
